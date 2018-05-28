f75c839a62a286a76268b41d63fae504a57c9e2a
Navin <navin@innovaturelabs.com>
2018-05-28 18:41:51 +0530

    Singleton

---
+++ src/main/java/com/codeseita/design/patterns/Singleton.java
@@ -0,0 +1,31 @@
+package com.codeseita.design.patterns;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author navin
+ */
+public class Singleton {
+
+    public static volatile Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInSingleton() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
